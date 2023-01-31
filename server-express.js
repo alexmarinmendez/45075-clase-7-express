8356644823c8b504d5759aa9c23ff711fc5328dc
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-30 19:41:29 -0500

    feat: add request params

--- server-express.js
+++ server-express.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 
-
+const users = [
+    { id: "1", name: "Shakira", gender: "F" },
+    { id: "2", name: "Piqué", gender: "M" },
+]
 
 
 
@@ -13,6 +16,9 @@ app.get('/', (request, response) => {
     response.send('Hola Mundo!')
 })
 
-app.get('/user', (request, response) => {
-    response.send({ user: 'Shakira' })
+app.get('/user/:id/:gender', (request, response) => {
+    const { id, gender } = request.params
+    const user = users.find(item => item.id === id)
+    if (!user) response.send({ error: "User not found", params: gender  })
+    else response.send({ user: user.name, params: gender })
 })
